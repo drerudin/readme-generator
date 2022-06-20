ee1c643445193d78e12170c0ab7f2ef7a8677e7b
Andrey Rudinskiy <drerudin@gmail.com>
2022-06-19 23:07:43 -0700

    added questions and generateMarkdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+  if (license !== 'none') {
+    return `![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)`  
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'none'){
+    return `\n [License](#license)\n`
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'none') {
+    return `## License
+    Project licensed by ${license}`
+  }
+  return ""
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+
+${data.description}
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## Contributing
+
+${data.contributions}
+
+## Tests
+To run test, type:
+
+${data.tests}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-
+const generateMarkdown = require('./Develop/utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -19,20 +19,34 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What are your installation instructions?',
-        name: 'install',
+        message: 'How do you use your application?',
+        name: 'usage',
         validate: (value)=>{ if(value){return true} else {return 'Give me something here to continue!'}},
     },
     {
         type: 'input',
-        message: 'What are your installation instructions?',
-        name: 'install',
+        message: 'Who helped you with this project?',
+        name: 'credits',
         validate: (value)=>{ if(value){return true} else {return 'Give me something here to continue!'}},
     },
+    {
+        type: 'list',
+        message: 'What license did you use for this project?',
+        name: 'license',
+        choices: ['MIT', 'gpl-3.0', 'apache-2.0', 'bsl-1.0', 'none']
+    },
+    {
+        type: 'checkbox',
+        message: 'Did you want other programmers to contribute?',
+        name: 'contributing',
+        choices: ['yes', 'no']
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+return fs.writeFileSync(path.join(process.cwd(), fileName),data)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
